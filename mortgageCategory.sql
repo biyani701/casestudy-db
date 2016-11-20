3accc2b75d111bcfa9adde03df954431a8a7a505
biyani701 <vishalbiyani@gmail.com>
2016-11-20 19:45:52 +0530

    First commit

---
+++ mortgageCategory.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS code_value;
+
+BEGIN;
+create table code_value(
+Type	char(50),
+Code	char(50),
+Description	char(50),
+Value	TEXT
+);
+
+INSERT INTO code_value values ('MortgageCategory', 'BTL', 'Buy To Let', 'a form of commercial mortgage used to purchase residential real estate with the intention of letting it to paying tenants');
+
---
+++ src/main/java/org/openshift/Generator.java
@@ -0,0 +1,12 @@
+package org.openshift;
+
+import java.util.Date;
+
+
+public class Generator {
+	public String generateValues() {
+		Date date = new Date();
+		return date.toString();
+	}
+
+}
